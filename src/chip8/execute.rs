5b91cbb8de01f0c10a688be16ca399fa52445159
Pavel Skrinnik <pavel.skrinnik@fundraiseup.com>
2025-06-03 19:01:33 +0200

    test: execute

--- src/chip8/execute.rs
+++ src/chip8/execute.rs
@@ -1,8 +1,5 @@
 use crate::{
-    chip8::{
-        instruction::{self, Chip8Instruction},
-        Chip8,
-    },
+    chip8::{instruction::Chip8Instruction, Chip8},
     display::Display,
 };
 
@@ -71,21 +68,80 @@ mod tests {
     use super::*;
     use crate::display::test_display::TestDisplay;
     use rstest::*;
+    use twelve_bit::u12::*;
 
     #[rstest]
-    #[case::set_display_pixel(&mut vec![true], 0, true, true)]
-    #[case::set_display_pixel(&mut vec![false], 0, true, false)]
-    #[case::set_display_pixel(&mut vec![false], 0, false, false)]
-    #[case::set_display_pixel(&mut vec![true], 0, false, false)]
+    fn test_clear_screen() {
+        let mut chip8 = get_test_chip8();
+        chip8.display_buffer = vec![true; 64 * 32];
+
+        chip8.execute(Chip8Instruction::ClearScreen());
+
+        assert_eq!(chip8.display_buffer, vec![false; 64 * 32]);
+    }
+
+    #[rstest]
+    #[case::jump(0x123, 0x123)]
+    fn test_jump(#[case] nnn: u16, #[case] expected: u16) {
+        let mut chip8 = get_test_chip8();
+        chip8.execute(Chip8Instruction::Jump(u12![nnn]));
+        assert_eq!(chip8.pc, u12![expected]);
+    }
+
+    #[rstest]
+    #[case::set_v_register(0x1, 0x12, 0x12)]
+    fn test_set_v_register(#[case] x: u8, #[case] nn: u8, #[case] expected: u8) {
+        let mut chip8 = get_test_chip8();
+        chip8.execute(Chip8Instruction::SetVRegister(x, nn));
+        assert_eq!(chip8.v_reg[x as usize], expected);
+    }
+
+    #[rstest]
+    /// Simple addition
+    #[case::add_v_register(0x0, 0x12, 0x12, 0x24)]
+    /// Addition with saturation
+    #[case::add_v_register(0x1, 0xF0, 0x20, 0xFF)]
+    fn test_add_v_register(
+        #[case] x: u8,
+        #[case] initial_value: u8,
+        #[case] nn: u8,
+        #[case] expected: u8,
+    ) {
+        let mut chip8 = get_test_chip8();
+        chip8.execute(Chip8Instruction::SetVRegister(x, initial_value));
+        chip8.execute(Chip8Instruction::AddVRegister(x, nn));
+        assert_eq!(chip8.v_reg[x as usize], expected);
+    }
+
+    #[rstest]
+    #[case::set_i_register(0x123, 0x123)]
+    fn test_set_i_register(#[case] nnn: u16, #[case] expected: u16) {
+        let mut chip8 = get_test_chip8();
+        chip8.execute(Chip8Instruction::SetIRegister(nnn));
+        assert_eq!(expected, chip8.i_reg);
+    }
+
+    #[rstest]
+    // true should be returned if the pixel is being turned off
+    #[case::set_display_pixel(&mut vec![true], 0, true, false, true)]
+    #[case::set_display_pixel(&mut vec![false], 0, true, true, false)]
+    #[case::set_display_pixel(&mut vec![false], 0, false, false, false)]
+    #[case::set_display_pixel(&mut vec![true], 0, false, true, false)]
     fn test_set_display_pixel(
         #[case] display_buffer: &mut Vec<bool>,
         #[case] px_idx: usize,
         #[case] px_val: bool,
-        #[case] expected: bool,
+        #[case] expected_px_value: bool,
+        #[case] expected_return_value: bool,
     ) {
         assert_eq!(
-            expected,
+            expected_return_value,
             Chip8::<TestDisplay>::set_display_pixel(display_buffer, px_idx, px_val)
         );
+        assert_eq!(expected_px_value, display_buffer[px_idx]);
+    }
+
+    fn get_test_chip8() -> Chip8<TestDisplay> {
+        Chip8::new(TestDisplay::new())
     }
 }
